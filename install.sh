b7e1abafd3506794cc00c46c96eaa9641448ad2a
Matthew Nekirov <matthew.nekirov@gmail.com>
2024-09-07 22:02:43 +0300

    fix: installation script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 set -ex
 
 git clone git@github.com:xDiaym/asm-template.git $1
-find . -not -path ".git/*" -not -path './build_*' -type f | xargs sed -i 's/lab1/$1/g'
+find . -not -path ".git/*" -not -path './build_*' -type f | xargs sed -i "s/lab1/${1}/g"
