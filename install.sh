a5c30001ad6420a004df8784846d5ed49b74540c
Matthew Nekirov <matthew.nekirov@gmail.com>
2024-09-08 21:15:46 +0300

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-git clone git@github.com:xDiaym/asm-template.git $1
+git clone https://github.com/xDiaym/asm-template.git $1
 find . -not -path ".git/*" -not -path './build_*' -type f | xargs sed -i "s/lab1/${1}/g"
